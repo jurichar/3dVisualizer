e114124a98bbc358cebe145ebce7344d42fd9e48
Julien Richard <45456851+jurichar@users.noreply.github.com>
2023-09-27 16:28:50 +0200

    Initial Commit

---
+++ 3DVizualizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  3DVizualizer
+//
+//  Created by Julien Richard on 27/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3DVizualizer/_DVizualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DVizualizerApp.swift
+//  3DVizualizer
+//
+//  Created by Julien Richard on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct _DVizualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
